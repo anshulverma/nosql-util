40390183e67bfa18fe62ba98994913d70ceb46de
ansverma <ansverma@adobe.com>
2015-05-18 17:40:38 -0700

    fix path of exported file

--- bin/run.sh
+++ bin/run.sh
@@ -9,4 +9,4 @@ CLASSPATH="$(hadoop classpath):$(hbase classpath):${NOSQL_UTIL_JAR}"
 HADOOP_CLASSPATH=${CLASSPATH} HBASE_CLASSPATH=${CLASSPATH} \
   hbase com.nuaavee.nosql.Driver export entity details_v2 "demo:8"
 
-hdfs dfs -text /tmp/entity_details/lowes/part-m-00000
+hdfs dfs -text /tmp/exported/entity/demo_8/part-m-00000
